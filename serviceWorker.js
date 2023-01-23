d5451e3b2e3c52ee137ccc4b1f41c0e2ee595001
unknown <sajadmhr666@gmail.com>
2023-01-23 14:55:28 +0330

    first commit

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,11 +1,11 @@
 const staticDevCoffee = "dev-coffee-site-v1"
 const assets = [
-  "/",
+  "task-manager-PWA",
   "index.html",
   "css/style.css",
   "js/app.js",
-  "assets/images/maskable_icon_x192.png",
-  "assets/images/maskable_icon_x512.png",
+  "./assets/images/maskable_icon_x192.png",
+  "./assets/images/maskable_icon_x512.png",
   "assets/fonts/Vazir.ttf",
   "assets/cheked.mp3",
   "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
